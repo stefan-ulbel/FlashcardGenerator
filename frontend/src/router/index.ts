ba88e6e86c577888ab8b3646e33fb4035632b870
Raoul Wograndl <wograndl.raoul@gmail.com>
2026-01-11 13:59:33 +0100

    config(frontend): set up unplugin-vue-router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: HomeView,
-    },
-    {
-      path: '/:catchAll(.*)*',
-      component: () => import('@/views/NotFoundView.vue'),
-    },
-  ],
+  routes,
 })
+
+// Updates routes at runtime without reloading the page.
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [VueRouter({}), vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
