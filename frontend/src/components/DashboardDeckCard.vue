f6e1fc22b627039fb9e49ab3c4ef36c33b6edaf6
Raoul Wograndl <wograndl.raoul@gmail.com>
2026-01-14 19:49:31 +0100

    feat(frontend): show deck cards in home page

---
+++ frontend/src/components/DashboardDeckCard.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import type { Deck } from '@/types/deck.ts'
+
+defineProps<{
+  deck: Deck
+}>()
+</script>
+
+<template>
+  <Card
+    class="flex flex-row items-center justify-between py-4 lg:flex-col lg:justify-start lg:py-6"
+  >
+    <CardHeader class="flex flex-1 items-center lg:w-full">
+      <RouterLink :to="`/decks/${deck.slug}`" class="flex items-center justify-start space-x-1">
+        <i-lucide-external-link class="text-sm" />
+        <span class="text-lg">{{ deck.title }}</span>
+      </RouterLink>
+    </CardHeader>
+    <CardContent class="lg:w-full lg:justify-start">
+      <Button class="cursor-pointer">Practice</Button>
+    </CardContent>
+  </Card>
+</template>
+
+<style scoped></style>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,9 +1,19 @@
 <script lang="ts" setup>
+import DashboardDeckCard from '@/components/DashboardDeckCard.vue'
+
 usePageStore().title = 'Home'
+
+const decksStore = useDecksStore()
+const { decks } = storeToRefs(decksStore)
+const { getDecks } = decksStore
+
+await getDecks()
 </script>
 
 <template>
-  <div></div>
+  <div v-if="decks" class="grid grid-cols-1 gap-4 lg:grid-cols-3">
+    <DashboardDeckCard v-for="deck in decks" :key="deck.title" :deck />
+  </div>
 </template>
 
 <style scoped></style>
