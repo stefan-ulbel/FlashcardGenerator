aeceae3e04361f1d9b71b58e37ef4b5a32b0abe7
Raoul Wograndl <wograndl.raoul@gmail.com>
2026-01-15 21:22:12 +0100

    feat(frontend): list flashcards in deck page

--- frontend/src/pages/decks/[slug].vue
+++ frontend/src/pages/decks/[slug].vue
@@ -23,7 +23,41 @@ watch(
 </script>
 
 <template>
-  <div></div>
+  <section v-if="deck">
+    <h2 class="text-3xl">Flashcards</h2>
+
+    <div class="class mt-6 grid gap-6 lg:grid-cols-2">
+      <Card v-for="flashcard in deck.flashcards" :key="flashcard.id">
+        <CardHeader class="flex items-center space-x-2">
+          <div
+            class="bg-muted text-sidebar-primary-foreground flex aspect-square size-10 items-center justify-center rounded-lg text-2xl font-bold"
+          >
+            Q
+          </div>
+          <span class="text-lg font-semibold">{{ flashcard.question }}</span>
+        </CardHeader>
+        <Separator />
+
+        <CardContent class="space-y-4">
+          <p
+            v-for="(answer, index) in flashcard.answers"
+            :key="index"
+            class="flex items-center space-x-3"
+          >
+            <span
+              :class="[index + 1 === flashcard.correctAnswer ? 'bg-green-400' : 'bg-red-400']"
+              class="text-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg text-xl font-bold"
+            >
+              {{ index + 1 }}
+            </span>
+            <span :class="{ 'font-semibold': index + 1 === flashcard.correctAnswer }">
+              {{ answer }}
+            </span>
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
